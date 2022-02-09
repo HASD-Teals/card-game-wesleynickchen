635267d457a851f6be6fd7ab34c5885bb896a5b6
wesleynickchen <wesley.nickchen@gmail.com>
2022-02-09 08:55:01 -0600

    done

--- src/CardGameClient.java
+++ src/CardGameClient.java
@@ -10,14 +10,31 @@ public class CardGameClient {
         //System.out.println( deck.toString());
         deck.shuffleCards();
         //System.out.println( deck.toString());
+        Deck deck1 = new Deck(suits, cols, 13, true, ranked);
+        deck1.CreateDeck();
+        deck1.shuffleCards();
 
         Scanner input = new Scanner(System.in);
         /*System.out.println("pick a card, any card (limit 1-52)");
         int p1 = input.nextInt();
         System.out.println("value is: " + deck.getCardAt(p1).getValue());*/
-        pickCard(input,deck);
-        pickCard(input,deck);
-
+        boolean p = false;
+        while(!p){
+            int p1 = pickCard(input,deck);
+            int p2 = pickCard(input,deck1);
+            if (p1 > p2) {
+                System.out.println("player 1 wins!");
+            } else if(p2 > p1) {
+                System.out.println("player 2 wins!");
+            }else{
+                System.out.println("its a tie!!");
+            }
+            System.out.println("play agian?");
+            String s = input.next();
+            if(!s.equals("yes")){
+                p = true;
+            }
+        }
         input.close();
     }
     public static int pickCard(Scanner in, Deck deck){
@@ -27,7 +44,7 @@ public class CardGameClient {
             System.out.println("pick a card, any playable card (limit 0-51)");
             int tempv = in.nextInt();
             if((tempv >= 0 && tempv <= 51) && deck.getCardAt(tempv).getPlayable()){
-                System.out.println("value is: " + deck.getCardAt(tempv).getValue());
+                System.out.println("its a(n): " + deck.getCardAt(tempv).getRank());
                 playInCoor= true;
                 deck.getCardAt(tempv).isPlayable(false);
                 retin = deck.getCardAt(tempv).getValue();
